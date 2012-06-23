44865353d2218a51dd3a7ec52fafd9dd9c188338
Mattias Andrée <maandree@operamail.com>
2012-06-23 19:43:22 +0200

    moving passes() into Parser form ParseTree

--- src/se/kth/maandree/jcbnfp/ParseTree.java
+++ src/se/kth/maandree/jcbnfp/ParseTree.java
@@ -236,7 +236,7 @@ public class ParseTree
     {
 	Return rc = new Return();
 	final GrammarElement grammar = assemble(def);
-	final int atom = passes(data, off, grammar);
+	final int atom = Parser.passes(data, off, grammar);
 	    
 	if (atom == -1) rc.read = -1;
 	if (atom >= 0)  rc.read = off + atom;
@@ -254,13 +254,13 @@ public class ParseTree
 	    final int end = start_end[1];
 		
 	    if (((JCBNFBacktrack)grammar).replacee != null)
-		rc.read = passes(data, off, start, end);
+		rc.read = Parser.passes(data, off, start, end);
 	    else
 	    {
 		final int[] replacee = Util.stringToIntArray(((JCBNFBacktrack)grammar).replacee);
 		final int[] replacer = Util.stringToIntArray(((JCBNFBacktrack)grammar).replacer);
 		    
-		rc.read = passes(data, off, start, end, replacee, replacer);
+		rc.read = Parser.passes(data, off, start, end, replacee, replacer);
 	    }
 		
 	    return rc.read < 0 ? null : rc;
@@ -437,183 +437,6 @@ public class ParseTree
 	    
 	return elem;
     }
-	
-	
-    /**
-     * Tests whether the data can pass a stored data chunk
-     * 
-     * @param   data   The data
-     * @param   off    The offset in the data
-     * @param   start  The start of the stored data chunk, inclusive
-     * @param   end    The end of the stored data chunk, exclusive
-     * @return         <code>-1</code> if it didn't pass, otherwise, the number of used characters
-     */
-    private int passes(final int[] data, final int off, final int start, final int end)
-    {
-	final int n = end - start;
-	
-	if (data.length - off < n)
-	    return -1;
-	
-	for (int i = 0; i < n; i++)
-	    if (data[i + off] != data[i + start])
-		return -1;
-	
-	return n;
-    }
     
-    
-    /**
-     * Tests whether the data can pass a stored data chunk, with replacement
-     * 
-     * @param   data      The data
-     * @param   off       The offset in the data
-     * @param   start     The start of the stored data chunk, inclusive
-     * @param   end       The end of the stored data chunk, exclusive
-     * @param   replacee  The replacement replacee
-     * @param   replacer  The replacement replacer
-     * @return            <code>-1</code> if it didn't pass, otherwise, the number of used characters
-     */
-    private int passes(final int[] data, final int off, final int start, final int end, final int[] replacee, final int[] replacer)
-    {
-	final boolean[] preset = new boolean[256];
-	final HashSet<Integer> set = new HashSet<Integer>();
-	    
-	outer:
-	    for (int j = start; j < end; j++)
-		if (data[j] == replacee[0]) //yes, this not that effecive, but who cares, compiling code should take hours
-		{
-		    final int n = replacee.length;
-		    if (j + n < data.length)
-			break;
-			
-		    for (int i = 0; i < n; i++)
-			if (data[j + i] != replacee[i])
-			    continue outer;
-			
-		    preset[j] = true;
-		    set.add(Integer.valueOf(j));
-		    j += replacee.length;
-		}
-	
-	int j = start;
-	for (int i = off, n = data.length; j < end; i++, j++)
-	{
-	    if (i >= n)
-		return -1;
-		
-	    if (preset[j & 255] && set.contains(new Integer(j)))
-	    {
-		for (int k = 0, m = replacer.length; k < m; k++, i++)
-		    if (data[i] != replacer[k])
-			return -1;
-		j += replacee.length;
-	    }
-	    
-	    if (data[i] != data[j])
-		return -1;
-	}
-	
-	return j - start;
-    }
-    
-    
-    /**
-     * Tests whether the data can pass an atomary grammar element
-     * 
-     * @param   data  The data
-     * @param   off   The offset in the data
-     * @param   def   The grammar element
-     * @return        <code>-1</code> if it didn't pass, <code>-2</code> if not atomary,
-     *                otherwise, the number of used characters
-     */
-    private int passes(final int[] data, final int off, final GrammarElement def)
-    {
-	if (def == null)
-	    return 0;
-	    
-	if (def instanceof JCBNFString)
-	{
-	    final int[] grammar = ((JCBNFString)def).string;
-	    final int n = grammar.length;
-	    final int m = data.length;
-		
-	    if (off + n >= m)
-		return -1;
-		
-	    for (int i = 0; i < n; i++)
-		if (data[i + off] != grammar[i])
-		    return -1;
-		
-	    return m;
-	}
-	if (def instanceof JCBNFWordString)
-	{
-	    final int[] grammar = ((JCBNFWordString)def).string;
-	    final int n = data.length;
-	    final int m = grammar.length;
-		
-	    if (off + n >= m)
-		return -1;
-		
-	    int prev = off == 0 ? -1 : data[off - 1];
-	    int next = off == n ? -1 : data[off];
-		
-	    if (JCBNFCheck.w.check(prev, next) == false)
-		return -1;
-		
-	    for (int i = 0; i < n; i++)
-		if (data[i + off] != grammar[i])
-		    return -1;
-		
-	    prev = off + m == 0 ? -1 : data[off + m - 1];
-	    next = off + m == n ? -1 : data[off + m];
-		
-	    if (JCBNFCheck.w.check(prev, next) == false)
-		return -1;
-		
-	    return m;
-	}
-	if (def instanceof JCBNFPartialString)
-	{
-	    final int[] grammar = ((JCBNFPartialString)def).string;
-	    final int n = grammar.length;
-	    final int m = data.length;
-		
-	    if (n == 0)
-		return 0;
-		
-	    if ((off == m) || (data[off] != grammar[0]))
-		return -1;
-		
-	    for (int i = 1; i < n; i++)
-		if ((i + off == m) || (data[i + off] != grammar[i]))
-		    return i;
-		
-	    return n;
-	}
-	if (def instanceof JCBNFCharacters)
-	{
-	    final JCBNFCharacters grammar = (JCBNFCharacters)def;
-	    final int n = data.length;
-	    
-	    if (off == n)
-		return -1;
-	    
-	    return grammar.contains(data[off]) ? 1 : -1;
-	}
-	if (def instanceof JCBNFCheck)
-	{
-	    final JCBNFCheck grammar = (JCBNFCheck)def;
-	    final int n = data.length;
-	    
-	    final int prev = off == 0 ? -1 : data[off - 1];
-	    final int next = off == n ? -1 : data[off];
-	    
-	    return grammar.check(prev, next) ? 0 : -1;
-	}
-	
-	return -2;
-    }
 }
 
