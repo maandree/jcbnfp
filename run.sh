04cccabe94d1f67ace588c6713f42cc05020b154
Mattias Andrée <maandree@operamail.com>
2012-07-12 16:48:52 +0200

    m

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ hasHome=0
 ## libraries
 jars=''
 if [ -d lib ]; then
-    jars=`echo $(find lib | grep .jar$) | sed -e 's/lib\//:/g' -e 's/ //g'`
+    jars=`echo $(find lib | grep .jar$) | sed -e 's/lib\//:lib\//g' -e 's/ //g'`
 fi
 
 
